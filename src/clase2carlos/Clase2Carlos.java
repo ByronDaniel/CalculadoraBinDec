792053b74d746dcc4c5f493dc667e835fde9dbb9
Byron Delgado <byrondanipm@gmail.com>
2022-10-29 15:45:23 -0500

    first commit

---
+++ src/clase2carlos/Clase2Carlos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase2carlos;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Clase2Carlos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
